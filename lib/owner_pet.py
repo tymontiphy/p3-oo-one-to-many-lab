2aeb30186f0b9eb2eacdf1e94f0b4870f0327306
Your Name <youremail@domain.com>
2024-12-05 20:06:34 +0300

    implement owner and pet classes with ful functionality test

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,42 @@
-class Pet:
-    pass
+# class Pet:
+#     pass
+
+# class Owner:
+#     pass
+
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("The pet must be an instance of Pet class")
+        pet.owner = self
+        if pet not in self._pets:
+            self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet type: {pet_type}")
+        self.pet_type = pet_type
+        if owner and not isinstance(owner, Owner):
+            raise Exception("The owner must be an instance of Owner class")
+        self.owner = owner
+        if owner:
+            owner.add_pet(self)
+        Pet.all.append(self)
+
